901e91a01d2c8534c97c3180c186d9bb564c5633
Matt Rudge <matt.rudge@codeinstitute.net>
2019-02-20 12:04:49 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# Weather API sample
+
+This uses the OpenWeatherMap.org API to retrieve the current weather and weather icon for cities around the world.
+
+A valid API key is required. The challenges are as follows:
+
+## REQUIRED:
+
+Add the following features to the current project:
+* The temperature in degrees Celsius (currently it is in Kelvin)
+* The current air pressure and wind speed
+* Style the data and present it nicely
+
+## BONUS ROUND:
+
+Use the documentation here: https://openweathermap.org/forecast5
+Create another button that displays a forecast for the city
+Think about the logic needed to do this. Can it be done with the current
+functions? Do they need to be duplicated?
+
+## OPTIONAL
+
+Add the sunset time for the chosen city. It is returned as a Unix timestamp.
+Research how to convert a Unix timestamp to a human-readable time format in JavaScript.
---
+++ weatherapi.js
@@ -0,0 +1,45 @@
+let request = new XMLHttpRequest();
+let APIKEY = "9b6e9334febd361b203e92c066725b28";
+
+function displayNicely(apiData) {
+    apiData = JSON.parse(apiData);
+    console.log(apiData);
+
+    let htmlString = apiData.name + " " + apiData.weather[0].description;
+    htmlString += `<img src="https://openweathermap.org/img/w/${apiData.weather[0].icon}.png">`;
+    document.getElementById("weatherData").innerHTML = htmlString;
+}
+
+function submitCity() {
+    let cityName = document.getElementById("cityForm")["city"].value;
+    request.open("GET", "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&APPID=" + APIKEY);
+    request.send();
+}
+
+
+request.onreadystatechange = function() {
+    if (this.readyState == 4) {
+        if (this.status == 200) {
+            displayNicely(this.responseText);
+        }
+        else if (this.status == 404) {
+            document.getElementById("weatherData").innerHTML = "<h2>City not found! Please try again.</h2>";
+        }
+    }
+}
+
+/* 
+Features we'd like to see:
+
+The temperature in degrees Celsius (currently it is in Kelvin)
+The current air pressure and wind speed
+The data to be styled and presented nicely.
+
+BONUS ROUND:
+
+Use the documentation here: https://openweathermap.org/forecast5
+Create another button that displays a forecast for the city
+Think about the logic needed to do this. Can it be done with the current
+functions? Do they need to be duplicated?
+
+*/
